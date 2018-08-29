b3d47747360379a84c034344041be7aba8ea9a9e
Lwq <943904454@qq.com>
2018-08-29 11:43:58 +0800

    重排序

---
+++ src/main/com/lwq/concurrentreorder/Test.java
@@ -0,0 +1,57 @@
+package main.com.lwq.concurrentreorder;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @Author: Lwq
+ * @Date: 2018/8/28 11:20
+ * @Version 1.0
+ * @Describe  重排序
+ */
+public class Test {
+    private static int x = 0, y = 0;
+    private static int a = 0, b = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        int i = 0;
+        for (; ; ) {
+            i++;
+            x = 0;
+            y = 0;
+            a = 0;
+            b = 0;
+            CountDownLatch latch = new CountDownLatch(1);
+
+            Thread one = new Thread(() -> {
+                try {
+                    latch.await();
+                } catch (InterruptedException e) {
+                }
+                a = 1;
+                x = b;
+            });
+
+            Thread other = new Thread(() -> {
+                try {
+                    latch.await();
+                } catch (InterruptedException e) {
+                }
+                b = 1;
+                y = a;
+            });
+            one.start();
+            other.start();
+            latch.countDown();
+            one.join();
+            other.join();
+
+            String result = "第" + i + "次 (" + x + "," + y + "）";
+            if (x == 0 && y == 0) {
+                System.err.println(result);
+                break;
+            } else {
+                System.out.println(result);
+            }
+        }
+    }
+}
